08972096ff852c48e078cdeace6fb79792d6eb56
Priyal Khurana <78503249+priyalkhurana@users.noreply.github.com>
2023-03-31 00:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ import secrets as secrets
 import time
 import os
 import toml
+from streamlit_chat import message
+from PIL import Image
+
+txtInputQuestion = "userQuestion"
+pageTitle = "Bhagvad Gita GPT"
 
 secrets = toml.load('secrets.toml')
 #api_key = secrets['openai.api_key']['pinecone_api_key']
@@ -115,18 +120,106 @@ if question != '':
     verse_strings = "".join(return_all_verses())
     prompt = f'''{header}\nQuestion:{question}\nVerses:\n{verse_strings}\nAnswer:\n'''
 
+    
+def generate_prompt(question):
+    prompt = f"I am here to help you. What is your question or problem? {question}"
+    return prompt
+
+def generate_chat_response(prompt):
     response = openai.Completion.create(
-        prompt = prompt,
-        **COMPLETIONS_API_PARAMS
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=1024,
+        temperature=0.7,
+        n=1,
+        stop=None,
+        timeout=20,
     )
-    # llm = OpenAI(streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
-    # resp = llm(prompt)
-    # st.markdown(resp)
-    # output.markdown(resp)
-    st.markdown(response["choices"][0]["text"].strip(" \n"))
-    st.markdown('\n\n')
-    st.markdown("Relevant verses:")
-    st.markdown(verse_strings.replace('\n','\n\n'))
+    message = response.choices[0].text.strip()
+    return message
+
+# Get user input
+question = st.text_input("Ask a question or describe your situation below, and then press Enter.")
+
+# Generate chat response
+if question:
+    prompt = generate_prompt(question)
+    message = generate_chat_response(prompt)
+    st.write("Bhagvad Gita says: ", message)
+
+
+def clear_text(textInput):
+
+    st.session_state[textInput] = ""
+
+def generate_response_davinci(question):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(question),
+        temperature=0.6,
+        max_tokens=2048
+    )
+    return response.choices[0].text
+
+def get_text():
+    input_text = st.text_input("Hello, ask me a question about life and philosophy.",placeholder="Type Your question here.", key=txtInputQuestion)
+    return input_text
+
+def page_setup(title, icon):
+    st.set_page_config(
+        page_title=title,
+        page_icon=icon,
+        layout='centered',
+        initial_sidebar_state='auto',
+        menu_items={
+            'About': 'About your application: **This is Bhagvad Gita GPT, a simple ChatGPT use case demo to show how one can easily leverage openAI APIs to create intelligent conversational experiences related to a specific topic.**'
+        }
+    )
+    st.sidebar.title('Creators :')
+    st.sidebar.markdown('PRIYAL KHURANA')
+    st.sidebar.write("DIVYANSH KUMAR")
+    st.sidebar.write("MITALI CHAUDHARY")
+
+    
+if __name__ == '__main__':
+
+    # Storing the chat
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+    icon = Image.open('gita.jpg')
+
+    #setup page
+    page_setup(pageTitle,icon)
+
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.title("Bhagvad Gita GPT")
+    with col2:
+        st.image(icon)
+    #st.write("test 1")
+
+    user_input = get_text()
+
+    print("get_text called.")
+    if user_input:
+        output = generate_response_davinci(user_input)
+
+        # store the output
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output)
+
+    #if st.session_state['generated']:
+
+    #st.markdown(response["choices"][0]["text"].strip(" \n"))
+    #st.markdown('\n\n')
+    #st.markdown("Relevant verses:")
+    #st.markdown(verse_strings.replace('\n','\n\n'))
+
 
 
 st.write('''\n\n Here's some examples of what you can ask:
